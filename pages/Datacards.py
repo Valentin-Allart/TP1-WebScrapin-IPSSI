dff1f32e0411ba6bece30e906b22fc7ec0239286
Valentin ALLART <allart.valentin78@gmail.com>
2023-12-11 16:57:53 +0100

    Warning no articles

--- pages/Datacards.py
+++ pages/Datacards.py
@@ -3,13 +3,20 @@ import pandas as pd
 
 st.set_page_config(page_title="TP1 - WebScraping", page_icon="🧊", layout="wide")
 
-df = pd.read_csv('articles.csv')
+try:
+    df = pd.read_csv('articles.csv')
+except:
+    df = pd.DataFrame()
 
 st.title("TP1 - Datacards")
 
-for index, row in df.iterrows():
-    st.title(f"**{row['Title']}**")
-    st.image(row['image'])
-    st.subheader(f"**{row['Description']}**")
-    st.write(f"**{row['Link']}**")
-    st.write('---')
+#try if the df is empty
+if df.empty:
+    st.warning("Aucun article trouvé")
+else:
+    for index, row in df.iterrows():
+        st.title(f"**{row['Title']}**")
+        st.image(row['image'])
+        st.subheader(f"**{row['Description']}**")
+        st.write(f"**{row['Link']}**")
+        st.write('---')
